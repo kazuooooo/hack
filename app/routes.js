9f5f5a1e59374c67451af0ae8da96520b39506ff
kazuooooo <matsumotokazuya7@gmail.com>
2018-01-14 13:48:59 +0900

    set taskpage spa

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import TasksPage from './containers/TasksPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/tasks" component={TasksPage} />
+      <Route exact path="/" component={TasksPage} />
     </Switch>
   </App>
 );
