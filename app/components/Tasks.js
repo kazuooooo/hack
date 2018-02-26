caca801b0d4a41d2620c6b19fd38a686db1653bb
matart15 <matart15@gmail.com>
2018-02-26 22:41:06 +0900

    default state for first time running

--- app/components/Tasks.js
+++ app/components/Tasks.js
@@ -10,7 +10,6 @@ import Task from '../components/Task';
 
 
 class Tasks extends Component {
-
   constructor(props) {
     super(props);
     this.state = { text: '' };
@@ -50,14 +49,14 @@ class Tasks extends Component {
                 console.log('callbackParams: ', this.props);
                 return {
                   clickButtonFunction: (a) => {
-                    console.log('clicked' , a)
+                    console.log('clicked', a);
                   },
                   actions: {
                     addTask: this.props.addTask,
                     deleteTask: this.props.deleteTask,
                     updateTask: this.props.updateTask
                   }
-                }
+                };
               }}
             />
           </div>
