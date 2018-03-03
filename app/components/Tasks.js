c6299f3fa4c8b8f0a9c7ccb3271867a54ba630a9
matart15 <matart15@gmail.com>
2018-03-03 15:37:46 +0900

    menu and file dialog open

--- app/components/Tasks.js
+++ app/components/Tasks.js
@@ -1,9 +1,15 @@
 // http://www.material-ui.com/
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import AppBar from 'material-ui/AppBar';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import { MuiThemeProvider } from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import Drawer from 'material-ui/Drawer';
+import FlatButton from 'material-ui/FlatButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import MenuItem from 'material-ui/MenuItem';
 import SortableTree from 'react-sortable-tree';
 import styles from './Tasks.css';
 import Task from '../components/Task';
@@ -11,7 +17,7 @@ import Task from '../components/Task';
 class Tasks extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { text: '', drawerOpen: false };
     this.handleChange = this.handleChange.bind(this);
     this.handleAddRootTask = this.handleAddRootTask.bind(this);
   }
@@ -35,24 +41,60 @@ class Tasks extends Component {
     );
   }
 
+  handleToggle = () => this.setState({ drawerOpen: !this.state.drawerOpen });
+
+  handleClose = () => this.setState({ drawerOpen: false });
+  handleImportFile = () => {
+    const { dialog } = require('electron').remote;
+    // FixMe currently electorn deialog can't use extensions only
+    // https://github.com/electron/electron/issues/11391
+    dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [
+        { name: 'test', extensions: ['json'] }
+      ]
+    }, (filename) => {
+      console.log(filename.toString());
+    });
+  }
+
   render() {
     return (
       <div>
         <MuiThemeProvider>
+          <Drawer
+            docked={false}
+            open={this.state.drawerOpen}
+            onRequestChange={(drawerOpen) => this.setState({ drawerOpen })}
+          >
+            <MenuItem onClick={this.handleClose}>Export data</MenuItem>
+            <MenuItem onClick={this.handleImportFile}>Import data</MenuItem>
+          </Drawer>
+          <AppBar
+            iconElementLeft={
+              <IconButton
+                onClick={this.handleToggle}
+              >
+                <NavigationMenu />
+              </IconButton>
+            }
+            iconElementRight={<FlatButton label="Save" />}
+            title="Hack"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+
           <div style={{ height: 1000 }}>
             <SortableTree
               treeData={this.props.tasks.treeData}
               onChange={newState => this.props.updateTasksState(newState)}
               nodeContentRenderer={Task}
-              generateNodeProps={() => {
-                return {
+              generateNodeProps={() => ({
                   actions: {
                     addTask: this.props.addTask,
                     deleteTask: this.props.deleteTask,
                     updateTask: this.props.updateTask
                   }
-                };
-              }}
+                })}
             />
           </div>
         </MuiThemeProvider>
