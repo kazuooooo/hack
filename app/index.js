6a4ce7434962b945c590c907043d3ad8212e73ed
kazuooooo <matsumotokazuya7@gmail.com>
2018-03-21 22:54:17 +0900

    adjust

--- app/index.js
+++ app/index.js
@@ -15,14 +15,14 @@ render(
   document.getElementById('root')
 );
 
-// if (module.hot) {
-//   module.hot.accept('./containers/Root', () => {
-//     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
-//     render(
-//       <AppContainer>
-//         <NextRoot store={store} history={history} />
-//       </AppContainer>,
-//       document.getElementById('root')
-//     );
-//   });
-// }
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
+    render(
+      <AppContainer>
+        <NextRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
